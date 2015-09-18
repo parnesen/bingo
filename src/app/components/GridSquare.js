12600d7b5aaf2cfd6f6972ab6e3611dbf376e8b6
parnesen <patrick.arnesen@gmail.com>
2015-09-18 09:22:27 -0700

    more cleanup

--- src/app/components/GridSquare.js
+++ src/app/components/GridSquare.js
@@ -1,13 +1,6 @@
 var React = require('react');
 var Bootstrap = require('react-bootstrap');
 
-var Navbar = Bootstrap.Navbar;
-var Nav = Bootstrap.Nav;
-var NavItem = Bootstrap.NavItem;
-var DropdownButton = Bootstrap.DropdownButton;
-var MenuItem = Bootstrap.MenuItem;
-
-
 var Button = Bootstrap.Button;
 
 module.exports = React.createClass({
